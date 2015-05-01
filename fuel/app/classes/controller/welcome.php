9fff93eac889f16e707bf36a3e2c24fdf8a09529
wizardry <rein1999@gmail.com>
2015-05-01 15:41:04 +0900

    fuelphp add

---
+++ fuel/app/classes/controller/welcome.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Fuel is a fast, lightweight, community driven PHP5 framework.
+ *
+ * @package    Fuel
+ * @version    1.7
+ * @author     Fuel Development Team
+ * @license    MIT License
+ * @copyright  2010 - 2013 Fuel Development Team
+ * @link       http://fuelphp.com
+ */
+
+/**
+ * The Welcome Controller.
+ *
+ * A basic controller example.  Has examples of how to set the
+ * response body and status.
+ *
+ * @package  app
+ * @extends  Controller
+ */
+// class Controller_Welcome extends Controller_Template
+// {
+// 	public $template = 'layouts/default';
+// }
+class Controller_Welcome extends Controller
+{
+	/**
+	 * The basic welcome message
+	 *
+	 * @access  public
+	 * @return  Response
+	 */
+	public function action_index()
+	{
+		$this->template->title = "ギタドラ スキルシミュレーター";
+		$this->template->content = View::forge('welcome/index');
+		// return Response::forge( View::forge('welcome/index') );
+	}
+
+	/**
+	 * A typical "Hello, Bob!" type example.  This uses a ViewModel to
+	 * show how to use them.
+	 *
+	 * @access  public
+	 * @return  Response
+	 */
+	public function action_hello()
+	{
+		return Response::forge(ViewModel::forge('welcome/hello'));
+	}
+
+	/**
+	 * The 404 action for the application.
+	 *
+	 * @access  public
+	 * @return  Response
+	 */
+	public function action_404()
+	{
+		return Response::forge(ViewModel::forge('welcome/404'), 404);
+	}
+
+}
---
+++ fuel/app/classes/view/layouts/template.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title><?php echo $title; ?></title>
+	<meta name="keywords" content="">
+	<meta name="description" content="">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="format-detection" content="telephone=no">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<meta property="og:image" content="">
+	<meta property="og:title" content="">
+	<meta property="og:type" content="website">
+	<meta property="og:url" content="">
+	<meta property="og:site_name" content="">
+	<meta property="fb:app_id" content="">
+	<meta property="og:description" content="">
+
+	<link rel="canonical" href="">
+	<link rel="stylesheet" href="">
+
+	<script src=""></script>
+</head>
+<body>
+	<?php echo $content; ?>
+</body>
+</html>
